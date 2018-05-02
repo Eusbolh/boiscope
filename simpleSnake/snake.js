9ba3f210248a67e093c84d14bbfec743fe466664
Cüneyt Emre Yavuz <cyavuz15@ku.edu.tr>
2018-05-02 22:01:22 +0300

    folder structure commit

--- simpleSnake/snake.js
+++ simpleSnake/snake.js
@@ -5,7 +5,8 @@ window.onload = function() {
     setInterval(game, 1000/15);
 }
 pause = false;
-previousState = "right"; 
+previousState = "right";
+oneFrameLater = true;
 px = py = 10;
 gs = tc = 20;
 ax = ay = 15;
@@ -14,21 +15,22 @@ yv = 0;
 trail = [];
 tail = 5;
 function game() {
+    oneFrameLater = true;
     if(!pause) {
         px += xv;
-        py += yv;
-    }
-    if(px < 0) {
-        px = tc-1;
-    }
-    if(px > tc-1) {
-        px = 0;
-    }
-    if(py < 0) {
-        py = tc-1;
-    }
-    if(py > tc-1) {
-        py = 0;
+        py += yv;      
+        if(px < 0) {
+            px = tc-1;
+        }
+        if(px > tc-1) {
+            px = 0;
+        }
+        if(py < 0) {
+            py = tc-1;
+        }
+        if(py > tc-1) {
+            py = 0;
+        }
     }
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canv.width, canv.height);
@@ -36,9 +38,8 @@ function game() {
     ctx.fillStyle = "lime";
     for(var i = 0; i < trail.length; i++) {
         ctx.fillRect(trail[i].x*gs, trail[i].y*gs, gs-2, gs-2);
-        if(trail[i].x == px && trail[i].y == py) {
+        if(trail[i].x == px && trail[i].y == py && !pause) {
             tail = 5;
-            setTimeout(function(){}, 2000);
         }
     }
     if(!pause)
@@ -51,39 +52,56 @@ function game() {
         valid = true;
         ax = Math.floor(Math.random()*tc);
         ay = Math.floor(Math.random()*tc);
+        valid = true
+        while(valid)
+            valid = false
+            for(var i = 0; i < trail.length; i++) {
+                if(trail[i].x == ax && trail[i].y == ay) {
+                    ax = Math.floor(Math.random()*tc);
+                    ay = Math.floor(Math.random()*tc);
+                    valid = true
+                    break;
+                }
+            }
     }
 
     ctx.fillStyle = "red";
     ctx.fillRect(ax*gs, ay*gs, gs-2, gs-2);
 }
 function keyPush(evt) {
-    switch(evt.keyCode) {
-        case 32:    // Pause
-            pause = !pause;
-            break;
-        case 37:    // Left
-            if(!pause && previousState != "right") {
-               xv = -1; yv = 0;
-               previousState = "left";
-            }
-            break;
-        case 38:    // Down
-            if(!pause && previousState != "up") {
-                xv = 0; yv = -1;
-                previousState = "down";
-            }
-            break;
-        case 39:    // Right
-            if(!pause && previousState != "left") {
-                xv = 1; yv = 0;
-                previousState = "right";
-            }
-            break;
-        case 40:    // Up
-            if(!pause && previousState != "down") {
-                xv = 0; yv = 1;
-                previousState = "up";
-            }
-            break;
-    }
+    if(oneFrameLater)
+        switch(evt.keyCode) {
+            case 32:    // Pause
+                oneFrameLater = false;
+                pause = !pause;
+                break;
+            case 37:    // Left
+                if(!pause && previousState != "right") {
+                    oneFrameLater = false;
+                    xv = -1; yv = 0;
+                    previousState = "left";
+                }
+                break;
+            case 38:    // Down
+                if(!pause && previousState != "up") {
+                    oneFrameLater = false;
+                    xv = 0; yv = -1;
+                    previousState = "down";
+                }
+                break;
+            case 39:    // Right
+                if(!pause && previousState != "left") {
+                    oneFrameLater = false;
+                    xv = 1; yv = 0;
+                    previousState = "right";
+                }
+                break;
+            case 40:    // Up
+                if(!pause && previousState != "down") {
+                    oneFrameLater = false;
+                    xv = 0; yv = 1;
+                    previousState = "up";
+                }
+                break;
+        }
 }
